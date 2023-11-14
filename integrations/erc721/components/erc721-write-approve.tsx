d2bdd21b8f90a1d3742d715a38f43ddbbed038ff
SZayShaK <sayedazaynabkazmi@gmail.com>
2023-11-14 03:26:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ integrations/erc721/components/erc721-write-approve.tsx
@@ -0,0 +1,81 @@
+import { useForm } from "react-hook-form"
+import { useDebounce } from "usehooks-ts"
+import { BaseError } from "viem"
+import { Address, useWaitForTransaction } from "wagmi"
+
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+import { ContractWriteButton } from "@/components/blockchain/contract-write-button"
+import { TransactionStatus } from "@/components/blockchain/transaction-status"
+
+import {
+  useErc721Approve,
+  usePrepareErc721Approve,
+} from "../generated/erc721-wagmi"
+
+interface Erc721WriteApproveProps {
+  address: Address
+}
+
+export function Erc721WriteApprove({ address }: Erc721WriteApproveProps) {
+  const { register, handleSubmit, watch } = useForm()
+  const watchToAddress: Address = watch("toAddress")
+  const watchTokenId: string = watch("tokenId")
+  const debouncedToAddress = useDebounce(watchToAddress, 500)
+  const debouncedTokenId = useDebounce(watchTokenId, 500)
+
+  const { config, error, isError } = usePrepareErc721Approve({
+    address,
+    args:
+      debouncedToAddress && debouncedTokenId
+        ? [debouncedToAddress, BigInt(debouncedTokenId)]
+        : undefined,
+    enabled: Boolean(debouncedToAddress && debouncedTokenId),
+  })
+
+  const { data, write, isLoading: isLoadingWrite } = useErc721Approve(config)
+
+  const { isLoading: isLoadingTx, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  })
+
+  const onSubmit = () => {
+    write?.()
+  }
+
+  return (
+    <Card>
+      <CardContent>
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+          <label>Address</label>
+          <input {...register("toAddress")} className="input" />
+          <label>Token ID</label>
+          <input type="number" {...register("tokenId")} className="input" />
+          <ContractWriteButton
+            isLoadingTx={isLoadingTx}
+            isLoadingWrite={isLoadingWrite}
+            loadingTxText="Approving..."
+            type="submit"
+            write={!!write}
+          >
+            Approve
+          </ContractWriteButton>
+          <TransactionStatus
+            error={error as BaseError}
+            hash={data?.hash}
+            isError={isError && Boolean(debouncedToAddress && debouncedTokenId)}
+            isLoadingTx={isLoadingTx}
+            isSuccess={isSuccess}
+          />
+        </form>
+      </CardContent>
+      <Separator className="my-4" />
+      <CardFooter className="justify-between">
+        <h3 className="text-center">ERC721 Approve</h3>
+        <p className="text-center text-sm text-muted-foreground">
+          Approve NFTs to any address
+        </p>
+      </CardFooter>
+    </Card>
+  )
+}
